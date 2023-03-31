28fffd7f0a8a03e69f3c03dd59c9eab9a4e82be8
TryHungury <mukolanadebernuy@gmail.com>
2023-03-31 13:30:25 +0300

    finish him

--- models/user.js
+++ models/user.js
@@ -12,14 +12,14 @@ const usersSchema = new Schema(
       required: [true, "Email is required"],
       unique: true,
     },
+    avatarURL: {
+      type: String,
+      required: false,
+    },
     subscription: {
       type: String,
       enum: ["starter", "pro", "business"],
       default: "starter",
-    },
-    avatarURL: {
-      type: String,
-      required: true,
     },
     token: {
       type: String,
